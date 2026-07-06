ea66e0275f0cda0f5765524da9dc0df6c7c116eb
Amit Kumar Sharma <ak.sharma9340038@gmail.com>
2026-07-06 11:39:35 +0530

    Add study timer feature with UI

--- script.js
+++ script.js
@@ -193,3 +193,60 @@ fetch(url, {
     author.textContent = data[0].author;
   })
   .catch((err) => console.error(err));
+
+// Study with me
+
+const studyImgBtn = document.querySelector(".timer");
+const studyBox = document.querySelector(".study-parent");
+const studyClose = document.querySelector(".study-close");
+
+studyImgBtn.addEventListener("click", () => {
+  studyBox.style.display = "block";
+});
+studyClose.addEventListener("click", () => {
+  studyBox.style.display = "none";
+});
+
+// study timer logic
+
+const display = document.querySelector(".display");
+let timer = null;
+let startTime = 0;
+let elapseTime = 0;
+let isRunning = false;
+
+function start() {
+  if (!isRunning) {
+    startTime = Date.now() - elapseTime;
+    timer = setInterval(update, 10);
+    isRunning = true;
+  }
+}
+function stop() {
+  if (isRunning) {
+    clearInterval(timer);
+    elapseTime = Date.now() - startTime;
+    isRunning = false;
+  }
+}
+function reset() {
+  clearInterval(timer);
+  startTime = 0;
+  elapseTime = 0;
+  isRunning = false;
+  display.textContent = "00:00:00:00";
+}
+function update() {
+  const currentTime = Date.now();
+  elapseTime = currentTime - startTime;
+  let hours = Math.floor(elapseTime / (1000 * 60 * 60));
+  let minutes = Math.floor((elapseTime / (1000 * 60)) % 60);
+  let seconds = Math.floor((elapseTime / 1000) % 60);
+  let milliseconds = Math.floor((elapseTime % 1000) / 10);
+  hours = String(hours).padStart(2, "0");
+  minutes = String(minutes).padStart(2, "0");
+  seconds = String(seconds).padStart(2, "0");
+  milliseconds = String(milliseconds).padStart(2, "0");
+
+  display.textContent = `${hours}:${minutes}:${seconds}:${milliseconds}`;
+}
