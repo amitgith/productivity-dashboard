ef341d3e25886f9dad5f0b9c3132f5a0cbdfbc4a
Amit Kumar Sharma <ak.sharma9340038@gmail.com>
2026-07-06 12:21:13 +0530

    Create daily planner UI

--- script.js
+++ script.js
@@ -215,38 +215,51 @@ let startTime = 0;
 let elapseTime = 0;
 let isRunning = false;
 
+const WORK_DURATION = 25 * 60;
+let remainingTime = WORK_DURATION * 1000;
+
 function start() {
   if (!isRunning) {
-    startTime = Date.now() - elapseTime;
+    startTime = Date.now();
     timer = setInterval(update, 10);
     isRunning = true;
   }
 }
-function stop() {
-  if (isRunning) {
-    clearInterval(timer);
-    elapseTime = Date.now() - startTime;
-    isRunning = false;
-  }
-}
-function reset() {
-  clearInterval(timer);
-  startTime = 0;
-  elapseTime = 0;
-  isRunning = false;
-  display.textContent = "00:00:00:00";
-}
+
 function update() {
-  const currentTime = Date.now();
-  elapseTime = currentTime - startTime;
-  let hours = Math.floor(elapseTime / (1000 * 60 * 60));
-  let minutes = Math.floor((elapseTime / (1000 * 60)) % 60);
-  let seconds = Math.floor((elapseTime / 1000) % 60);
-  let milliseconds = Math.floor((elapseTime % 1000) / 10);
-  hours = String(hours).padStart(2, "0");
+  const elapsedTime = Date.now() - startTime;
+  const timeLeft = remainingTime - elapsedTime;
+
+  if (timeLeft <= 0) {
+    clearInterval(timer);
+    isRunning = false;
+    display.textContent = "00:00";
+    alert("🎉 Time's up! Take a break.");
+    return;
+  }
+
+  let minutes = Math.floor(timeLeft / (1000 * 60));
+  let seconds = Math.floor((timeLeft / 1000) % 60);
+
   minutes = String(minutes).padStart(2, "0");
   seconds = String(seconds).padStart(2, "0");
-  milliseconds = String(milliseconds).padStart(2, "0");
 
-  display.textContent = `${hours}:${minutes}:${seconds}:${milliseconds}`;
+  display.textContent = `${minutes}:${seconds}`;
 }
+
+function reset() {
+  clearInterval(timer);
+  isRunning = false;
+
+  remainingTime = WORK_DURATION * 1000;
+  display.textContent = "25:00";
+}
+
+// daily clicked button overlay
+
+const dailyImgBtn = document.querySelector(".daily");
+const dailyBox = document.querySelector(".daily-parent");
+
+dailyImgBtn.addEventListener("click", () => {
+  dailyBox.style.display = "block";
+});
